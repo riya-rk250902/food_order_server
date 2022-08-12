6f0239ad530fad55309e04a4e99e152d87c7ef35
SHIVSB <2020kuec2005@iiitkota.ac.in>
2022-08-12 21:30:05 +0530

    cors error removed

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const logger = require("morgan"); 
+const logger = require("morgan");
 const app = express();
 const PORT = 4000;
 const dbConfig = require("./services/dbConfig");
-const router = require("./routes/index")
+const router = require("./routes/index");
 const cors = require("cors");
+
 app.use(logger("dev"));
-app.use(express.json({ limit: "50mb"}));
-app.use(express.urlencoded({ limit: "50mb", extended: false}));
-app.use("/api", router);
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use("/api", router);
 dbConfig();
 
-app.listen(PORT, function(error){
+app.listen(PORT, function (error) {
+  if (error) {
+    console.log("error in starting the server");
+  }
 
-    if(error){
-
-        console.log("error in starting the server");
-
-    }
-
-    console.log(`server started successfully on port : ${PORT}`);
-
+  console.log(`server started successfully on port : ${PORT}`);
 });
